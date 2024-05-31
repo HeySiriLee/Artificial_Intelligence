bcc39549348a2ac174bcd38fb2a553c732e85548
HeySiriLee <eunsilling@gmail.com>
2024-05-31 17:08:37 +0900

    교수님수업클론코딩듕

---
+++ 0531/nextjs/app/components/inc_header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <>
+      <header className="text-gray-600 body-font">
+        {/* <img src= */}
+        <p>Hello</p>
+      </header>
+      <footer />
+    </>
+  );
+}
---
+++ 0531/nextjs/app/main/page.js
@@ -0,0 +1,17 @@
+import Header from "../components/inc_header";
+import Footer from "../components/inc_footer";
+
+export default function Main() {
+  return (
+    <>
+      <Header />
+      <section>
+        <p>Nice to meet you:)</p>
+      </section>
+      <section>
+        <p>Nice to meet you:)</p>
+      </section>
+      <Footer />
+    </>
+  );
+}
